0d75cd69a508a34c08307fcc121982635034d1af
Olemis Lang <olemis+work@gmail.com>
2017-01-12 13:11:53 -0500

    [home] refs #1 - Group meetups in sections according to event date

--- assets/js/meetup.ctl.js
+++ assets/js/meetup.ctl.js
@@ -4,6 +4,30 @@ var meetupApp = angular.module('MeetupApp', []);
 meetupApp.controller('MeetupCtl', 
     function($scope) {
       $scope.events = meetup_data.events;
+      $scope.future = [];
+      $scope.past = [];
+      $scope.today = [];
+
+      var events = meetup_data.events,
+          timeline = meetup_data.timeline,
+          now = new Date(),
+          today_am = new Date(now.getYear() + 1900, now.getMonth(), now.getDate(), 0, 0, 0),
+          today_pm = new Date(now.getYear() + 1900, now.getMonth(), now.getDate(), 23, 59, 59);
+      for (var i = 0; i < timeline.length; ++i) {
+        var meetup = events['ev_' + timeline[i]];
+        var queue;
+        if (meetup.date < today_am) {
+          queue = $scope.past;
+        }
+        else if (meetup.date > today_pm) {
+          queue = $scope.future;
+        }
+        else {
+          queue = $scope.today;
+        }
+        queue.push(meetup)
+      }
+
       $scope.timeline = meetup_data.timeline;
       $scope.sources = meetup_data.sources;
     }
